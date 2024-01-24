b70e63599074cf9a57a7e7d10314dbb959189569
ArpitShukla3 <arpit900shukla@gmail.com>
2024-01-24 23:34:51 +0530

    4th commit

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
-const PORT=4500;
-import app from "./app.js";
-
 import bodyParser from "body-parser";
-
+import express from "express";
+import route from "./Route/Router.js"
+import cors from "cors";
+import dataBaseConnect from "./config/dataBaseConnection.js"
+dataBaseConnect();
+const app=express();
+app.use(express.json());
 app.use(bodyParser.json());
 
+app.use(bodyParser.json());
+app.use(cors());
+app.use("/show",route);
+app.use("/",(req,res)=>{
+    return res.status(200).json({
+        success:true,
+        message:"i am working"
+    })
+})
 
-
-app.listen(PORT,()=>
+app.listen(process.env.PORT,()=>
 {
-    console.log("Server is listening at port: "+PORT);
+    console.log("Server is listening at port: "+process.env.PORT);
 })  
  
 
