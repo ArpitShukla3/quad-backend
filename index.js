751d6db8701de2dd7b162c632ff0ae1b607ed9ff
ArpitShukla3 <arpit900shukla@gmail.com>
2024-01-25 10:19:05 +0530

    new commit

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import express from "express";
 import route from "./Route/Router.js"
 import cors from "cors";
 import dataBaseConnect from "./config/dataBaseConnection.js"
+import { databaseSetData, sendData } from "./Controller/Controller.js";
 dataBaseConnect();
 const app=express();
 app.use(express.json());
@@ -10,14 +11,13 @@ app.use(bodyParser.json());
 
 app.use(bodyParser.json());
 app.use(cors());
-app.use("/show",route);
+app.use("/show",databaseSetData,sendData);
 app.use("/",(req,res)=>{
     return res.status(200).json({
         success:true,
         message:"i am working"
     })
 })
-
 app.listen(process.env.PORT,()=>
 {
     console.log("Server is listening at port: "+process.env.PORT);
